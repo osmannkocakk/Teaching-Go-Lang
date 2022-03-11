dbe2f1ef4c55e3967efe6ccb575a4c01b760edbb
osman.kocak <osman.kocak@bdh.com.tr>
2022-03-11 13:01:03 +0300

    data types examples

---
+++ datatypes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int = 1
+	var pi32 float32 = 3.14159265           //Output 3.1415927
+	var pi64 float64 = 3.141592653589793238 //Output 3.141592653589793
+	var name string = "Osman Kocak"
+	var isActive bool = true
+
+	fmt.Println(x)
+	fmt.Println(pi32)
+	fmt.Println(pi64)
+	fmt.Println(name)
+	fmt.Println(isActive)
+}
